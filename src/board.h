a40d5e8a07e8c41c972fb65900419b6b1a3a9d54
Anton Krug <anton.krug@gmail.com>
2022-06-12 15:43:49 +0100

    Second re-balancing (division by 2)

--- src/board.h
+++ src/board.h
@@ -18,43 +18,43 @@ namespace cong {
   class Board {
   public:
     const std::array<cong::Tile::Basic *, 28> tiles = {{
-      new cong::Tile::Utility("Post Office", "Post", 75u, 2u, 5u, 38u),
-      new cong::Tile::Utility("Internet",    "Inet", 75u, 2u, 5u, 38u),
+      new cong::Tile::Utility("Post Office", "Post", 37u, 1u, 3u, 19u),
+      new cong::Tile::Utility("Internet",    "Inet", 37u, 1u, 3u, 19u),
 
-      new cong::Tile::Province("Munster",    "P:Mu", 100u, {{ 13u, 25u, 50u, 100u, 0u }}, 50u),
-      new cong::Tile::Province("Connaught",  "P:Co", 100u, {{ 13u, 25u, 50u, 100u, 0u }}, 50u),
-      new cong::Tile::Province("Leinster",   "P:Le", 100u, {{ 13u, 25u, 50u, 100u, 0u }}, 50u),
-      new cong::Tile::Province("Ulster",     "P:Ul", 100u, {{ 13u, 25u, 50u, 100u, 0u }}, 50u),
+      new cong::Tile::Province("Munster",    "P:Mu",  50u, {{ 7u, 13u, 25u, 50u, 0u }}, 25u),
+      new cong::Tile::Province("Connaught",  "P:Co",  50u, {{ 7u, 13u, 25u, 50u, 0u }}, 25u),
+      new cong::Tile::Province("Leinster",   "P:Le",  50u, {{ 7u, 13u, 25u, 50u, 0u }}, 25u),
+      new cong::Tile::Province("Ulster",     "P:Ul",  50u, {{ 7u, 13u, 25u, 50u, 0u }}, 25u),
 
-      new cong::Tile::County("Westmeath",    "C:WH", cong::Tile::Color::Brown,   30u, {{  1u,   5u,  15u,   45u,   80u,  125u }},  25u,  25u,  15u),
-      new cong::Tile::County("Donegal",      "C:DL", cong::Tile::Color::Brown,   30u, {{  2u,  10u,  30u,   90u,  160u,  225u }},  25u,  25u,  15u),
+      new cong::Tile::County("Westmeath",    "C:WH", cong::Tile::Color::Brown,   15u, {{  1u,   3u,   7u,   22u,   40u,   62u }},  12u,  12u,  7u),
+      new cong::Tile::County("Donegal",      "C:DL", cong::Tile::Color::Brown,   15u, {{  2u,   5u,  15u,   45u,   80u,  112u }},  12u,  12u,  7u),
 
-      new cong::Tile::County("Sligo",        "C:SO", cong::Tile::Color::Blue,    50u, {{  3u,  15u,  45u,  135u,  200u,  230u }},  25u,  25u,  25u),
-      new cong::Tile::County("Down",         "C:DN", cong::Tile::Color::Blue,    50u, {{  3u,  15u,  45u,  135u,  200u,  230u }},  25u,  25u,  25u),
-      new cong::Tile::County("Laois",        "C:LS", cong::Tile::Color::Blue,    60u, {{  4u,  20u,  50u,  150u,  225u,  300u }},  25u,  25u,  30u),
+      new cong::Tile::County("Sligo",        "C:SO", cong::Tile::Color::Blue,    25u, {{  2u,   7u,  22u,   67u,  100u,  115u }},  13u,  13u,  12u),
+      new cong::Tile::County("Down",         "C:DN", cong::Tile::Color::Blue,    25u, {{  2u,   8u,  23u,   68u,  100u,  116u }},  13u,  13u,  13u),
+      new cong::Tile::County("Laois",        "C:LS", cong::Tile::Color::Blue,    30u, {{  3u,  10u,  25u,   75u,  112u,  150u }},  13u,  13u,  15u),
 
-      new cong::Tile::County("Tyrone",       "C:TE", cong::Tile::Color::Pink,    70u, {{  5u,  25u,  75u,  225u,  313u,  375u }},  50u,  50u,  35u),
-      new cong::Tile::County("Cavan",        "C:CN", cong::Tile::Color::Pink,    70u, {{  5u,  25u,  75u,  225u,  313u,  375u }},  50u,  50u,  35u),
-      new cong::Tile::County("Kerry",        "C:KY", cong::Tile::Color::Pink,    80u, {{  6u,  30u,  90u,  250u,  350u,  450u }},  50u,  50u,  40u),
+      new cong::Tile::County("Tyrone",       "C:TE", cong::Tile::Color::Pink,    35u, {{  3u,  12u,  37u,  112u,  156u,  187u }},  25u,  25u,  17u),
+      new cong::Tile::County("Cavan",        "C:CN", cong::Tile::Color::Pink,    35u, {{  3u,  13u,  38u,  113u,  157u,  188u }},  25u,  25u,  18u),
+      new cong::Tile::County("Kerry",        "C:KY", cong::Tile::Color::Pink,    40u, {{  4u,  15u,  45u,  125u,  175u,  225u }},  25u,  25u,  20u),
 
-      new cong::Tile::County("Fermanagh",    "C:FE", cong::Tile::Color::Orange,  90u, {{  7u,  35u, 100u,  225u,  375u,  475u }},  50u,  50u,  45u),
-      new cong::Tile::County("Leitrim",      "C:LM", cong::Tile::Color::Orange,  90u, {{  7u,  35u, 100u,  225u,  375u,  475u }},  50u,  50u,  45u),
-      new cong::Tile::County("Galway",       "C:G ", cong::Tile::Color::Orange, 100u, {{  8u,  40u, 110u,  300u,  400u,  500u }},  50u,  50u,  50u),
+      new cong::Tile::County("Fermanagh",    "C:FE", cong::Tile::Color::Orange,  45u, {{  4u,  17u,  50u,  112u,  187u,  237u }},  25u,  25u,  22u),
+      new cong::Tile::County("Leitrim",      "C:LM", cong::Tile::Color::Orange,  45u, {{  4u,  18u,  50u,  113u,  188u,  238u }},  25u,  25u,  23u),
+      new cong::Tile::County("Galway",       "C:G ", cong::Tile::Color::Orange,  50u, {{  5u,  20u,  55u,  150u,  200u,  250u }},  25u,  25u,  25u),
 
-      new cong::Tile::County("Meath",        "C:MH", cong::Tile::Color::Red,    110u, {{  9u,  45u, 125u,  350u,  437u,  525u }},  75u,  75u,  55u),
-      new cong::Tile::County("Kilkenny",     "C:KK", cong::Tile::Color::Red,    110u, {{  9u,  45u, 125u,  350u,  437u,  525u }},  75u,  75u,  55u),
-      new cong::Tile::County("Offaly",       "C:OY", cong::Tile::Color::Red,    120u, {{ 10u,  50u, 150u,  375u,  461u,  550u }},  75u,  75u,  60u),
+      new cong::Tile::County("Meath",        "C:MH", cong::Tile::Color::Red,     55u, {{  5u,  22u,  62u,  175u,  218u,  262u }},  37u,  37u,  27u),
+      new cong::Tile::County("Kilkenny",     "C:KK", cong::Tile::Color::Red,     55u, {{  5u,  23u,  63u,  175u,  219u,  263u }},  37u,  37u,  28u),
+      new cong::Tile::County("Offaly",       "C:OY", cong::Tile::Color::Red,     60u, {{  6u,  25u,  75u,  187u,  230u,  275u }},  37u,  37u,  30u),
 
-      new cong::Tile::County("Mayo",         "C:MO", cong::Tile::Color::Yellow, 130u, {{ 11u,  55u, 165u,  400u,  487u,  575u }},  75u,  75u,  65u),
-      new cong::Tile::County("Dublin",       "C:D ", cong::Tile::Color::Yellow, 130u, {{ 11u,  55u, 165u,  400u,  487u,  575u }},  75u,  75u,  65u),
-      new cong::Tile::County("Louth",        "C:LH", cong::Tile::Color::Yellow, 140u, {{ 12u,  60u, 180u,  425u,  512u,  600u }},  75u,  75u,  70u),
+      new cong::Tile::County("Mayo",         "C:MO", cong::Tile::Color::Yellow,  65u, {{  6u,  27u,  82u,  200u,  243u,  287u }},  38u,  38u,  32u),
+      new cong::Tile::County("Dublin",       "C:D ", cong::Tile::Color::Yellow,  65u, {{  6u,  28u,  83u,  200u,  244u,  288u }},  38u,  38u,  33u),
+      new cong::Tile::County("Louth",        "C:LH", cong::Tile::Color::Yellow,  70u, {{  7u,  30u,  90u,  226u,  256u,  300u }},  38u,  38u,  35u),
 
-      new cong::Tile::County("Wexford",      "C:WX", cong::Tile::Color::Green,  150u, {{ 13u,  65u, 195u,  450u,  550u,  638u }}, 100u, 100u,  75u),
-      new cong::Tile::County("Limerick",     "C:L ", cong::Tile::Color::Green,  150u, {{ 13u,  65u, 195u,  450u,  550u,  638u }}, 100u, 100u,  75u),
-      new cong::Tile::County("Waterford",    "C:WD", cong::Tile::Color::Green,  160u, {{ 14u, 125u, 220u,  500u,  600u,  700u }}, 100u, 100u,  80u),
+      new cong::Tile::County("Wexford",      "C:WX", cong::Tile::Color::Green,   75u, {{  7u,  32u,  92u,  225u,  275u,  319u }},  50u,  50u,  37u),
+      new cong::Tile::County("Limerick",     "C:L ", cong::Tile::Color::Green,   75u, {{  7u,  33u,  93u,  226u,  278u,  320u }},  50u,  50u,  38u),
+      new cong::Tile::County("Waterford",    "C:WD", cong::Tile::Color::Green,   80u, {{  8u,  40u, 110u,  250u,  300u,  350u }},  50u,  50u,  40u),
 
-      new cong::Tile::County("Cork",         "C:C ", cong::Tile::Color::Navy,   175u, {{ 17u,  87u, 250u,  600u,  650u,  750u }}, 100u, 100u,  88u),
-      new cong::Tile::County("Roscommon",    "C:RN", cong::Tile::Color::Navy,   200u, {{ 20u, 100u, 300u,  650u,  700u,  850u }}, 100u, 100u, 100u),
+      new cong::Tile::County("Cork",         "C:C ", cong::Tile::Color::Navy,    87u, {{  9u,  43u, 125u,  300u,  325u,  375u }},  50u,  50u,  44u),
+      new cong::Tile::County("Roscommon",    "C:RN", cong::Tile::Color::Navy,   100u, {{ 10u,  50u, 150u,  325u,  350u,  425u }},  50u,  50u,  50u),
     }};
 
     Board();
--- src/game.h
+++ src/game.h
@@ -26,23 +26,23 @@ namespace cong {
         cong::Card("Advance to Roscommon.", false, 0),
         // TODO advance to a tile
 
-        cong::Card("Advance to Tyrone. If you pass Go, collect 100k$.", false, 0, [] (cong::Game &game) {
+        cong::Card("Advance to Tyrone. If you pass Go, collect 50k$.", false, 0, [] (cong::Game &game) {
           //TODO: need access to boardsearch, not just board
           if (game.currentPlayer->getPosition() > 10) {
-            game.currentPlayer->moneyFromBank(100);
+            game.currentPlayer->moneyFromBank(50);
           }
           game.currentPlayer->moveToPosition(10);
           // TODO: trigger board action, might need Game class access, to pick cards, whatever, might never happen
         }),
 
-        cong::Card("Advance to Offaly. If you pass Go, collect 100k$.", false, 0, [] (cong::Game &game) {
+        cong::Card("Advance to Offaly. If you pass Go, collect 50k$.", false, 0, [] (cong::Game &game) {
           if (game.currentPlayer->getPosition() > 20) {
-            game.currentPlayer->moneyFromBank(100);
+            game.currentPlayer->moneyFromBank(50);
           }
           game.currentPlayer->moveToPosition(20);
         }),
 
-        cong::Card("Pay private school fees of 75k$.", false, -75),
+        cong::Card("Pay private school fees of 37k$.", false, -37),
 
         cong::Card("Get out of Jail card. This card may be kept until needed or sold.", true, 0, [] (cong::Game &game) {
           // TODO: does player need state for being stuck in jail? So we can unstuck him?
@@ -59,36 +59,36 @@ namespace cong {
           // Do i need to trigger tile action?
         }),
 
-        cong::Card("Advance to Go and collect 100k$.", false, 100, [] (cong::Game &game) {
+        cong::Card("Advance to Go and collect 50k$.", false, 50, [] (cong::Game &game) {
           game.currentPlayer->moveToPosition(0);
         }),
 
-        cong::Card("Sell your shares for a profit. Collect 75k$.", false, 75),
+        cong::Card("Sell your shares for a profit. Collect 37k$.", false, 37),
     };
 
 
     std::deque<cong::Card> cardsCommunity = {
-        cong::Card("You win tickets to a sold out All-Ireland final and sell for a huge profit. Collect 10k$.", false, 10),
+        cong::Card("You win tickets to a sold out All-Ireland final and sell for a huge profit. Collect 5k$.", false, 5),
 
-        cong::Card("Advance to Go. Collect 100k$.", false, 100),
+        cong::Card("Advance to Go. Collect 50k$.", false, 50),
         // TODO: should the trigger on the Go tile give the money instead?
 
-        cong::Card("Pay 25k$ for a 4-star weekend spa break in Donegal.", false, -25),
+        cong::Card("Pay 12k$ for a 4-star weekend spa break in Donegal.", false, -12),
 
         cong::Card("Advance to Westmeath.", false, 0),
 
-        cong::Card("Collect 50k$ profits for chartering your private helicopter.", false, 50),
+        cong::Card("Collect 25k$ profits for chartering your private helicopter.", false, 25),
 
-        cong::Card("Your colleagues hire your holiday home for a week. Collect 100$ from each player.", false, 0),
+        cong::Card("Your colleagues hire your holiday home for a week. Collect 2k$ from each player.", false, 0),
         // TODO access to the Game state and all players
         // TODO can that trigger negative on non-active player, support auction for non-playing player
         // TODO prepare for long line text of the cards // support multiline?
 
-        cong::Card("Your car insurance claim is settled. Collect 12k$", false, 12),
+        cong::Card("Your car insurance claim is settled. Collect 6k$", false, 6),
 
         cong::Card("Get out of Jail card. This card may be kept until needed or sold.", true, 0),
 
-        cong::Card("You are investigated for identity fraud. Go to jail. Move directly to jail. Do not pass go. Do not collect 100k$.", true, 0),
+        cong::Card("You are investigated for identity fraud. Go to jail. Move directly to jail. Do not pass go. Do not collect 50k$.", true, 0),
     };
 
     void shuffleCards();
